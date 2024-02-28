80847fce7cf2c70155ca230cdae137f931a7ceec
simps4simps <savarimani6@gmail.com>
2024-02-28 18:21:14 +0330

    Added icons

---
+++ src/components/IconsExport.tsx
@@ -0,0 +1,13 @@
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import InboxIcon from "@mui/icons-material/Inbox";
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+export {
+  SpaceDashboardIcon,
+  InboxIcon,
+  PersonSearchIcon,
+  AssignmentIndIcon,
+  SettingsIcon,
+};
