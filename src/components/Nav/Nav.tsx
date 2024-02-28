e04aaa8a76c188492d4b0f27b152a99cd41d15c3
simps4simps <savarimani6@gmail.com>
2024-02-28 19:02:04 +0330

    Added tool tips

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -12,41 +12,43 @@ import { Link, Outlet } from "react-router-dom";
 const Nav = () => {
   return (
     <>
-      <Link to={"/"}>
-        <div className="links">
-          <SpaceDashboardIcon />
-          <p>Dashboard</p>
-        </div>
-      </Link>
+      <div className="nav-wrapper">
+        <Link to={"/"}>
+          <div className="links" title="Dashboard">
+            <SpaceDashboardIcon />
+            <p>Dashboard</p>
+          </div>
+        </Link>
 
-      <Link to={"/archives"}>
-        <div className="links">
-          <InboxIcon />
-          <p>Archive</p>
-        </div>
-      </Link>
+        <Link to={"/archives"}>
+          <div className="links" title="Archives">
+            <InboxIcon />
+            <p>Archive</p>
+          </div>
+        </Link>
 
-      <Link to={"/person-search"}>
-        <div className="links">
-          <PersonSearchIcon />
-          <p>Person search</p>
-        </div>
-      </Link>
+        <Link to={"/person-search"}>
+          <div className="links" title="Search">
+            <PersonSearchIcon />
+            <p>Search</p>
+          </div>
+        </Link>
 
-      <Link to={"/assign"}>
-        <div className="links">
-          <AssignmentIndIcon />
-          <p>Assign</p>
-        </div>
-      </Link>
+        <Link to={"/assign"}>
+          <div className="links" title="Assign">
+            <AssignmentIndIcon />
+            <p>Assign</p>
+          </div>
+        </Link>
 
-      <Link to={"/settings"}>
-        <div className="links" id="settings-link">
-          <SettingsIcon />
-        </div>
-      </Link>
+        <Link to={"/settings"}>
+          <div className="links" id="settings-link" title="Settings">
+            <SettingsIcon />
+          </div>
+        </Link>
 
-      <Outlet />
+        <Outlet />
+      </div>
     </>
   );
 };
