3c40a604e61ac885f2271742f5a9e916a0e1ddfb
simps4simps <savarimani6@gmail.com>
2024-02-28 18:21:00 +0330

    Added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Nav from "./components/Nav/Nav";
+import Archive from "./components/Archive/Archive";
+import PersonSearch from "./components/PersonSearch/PersonSearch";
+import Assign from "./components/Assign/Assign";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div id="app">
+      <div className="wrapper">
+        <BrowserRouter>
+          <nav id="left-navigation">
+            <Nav />
+          </nav>
 
-export default App
+          <div id="main-display">
+            <Routes>
+              <Route path="/" index Component={Dashboard} />
+              <Route path="/archives" index Component={Archive} />
+              <Route path="/person-search" index Component={PersonSearch} />
+              <Route path="/assign" index Component={Assign} />
+              <Route path="/settings" index Component={Settings} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+};
+
+export default App;
