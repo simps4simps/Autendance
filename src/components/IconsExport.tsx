4590b44e3b4992893c2157a53d11b9a992ff7665
simps4simps <savarimani6@gmail.com>
2024-03-02 23:36:57 +0330

    Added close icon

--- src/components/IconsExport.tsx
+++ src/components/IconsExport.tsx
@@ -5,6 +5,7 @@ import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
 import MenuIcon from "@mui/icons-material/Menu";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
+import CloseIcon from "@mui/icons-material/Close";
 
 export {
   SpaceDashboardIcon,
@@ -14,4 +15,5 @@ export {
   SettingsIcon,
   MenuIcon,
   LogoutIcon,
+  CloseIcon,
 };
