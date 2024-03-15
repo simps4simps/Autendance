44f477049d3b45f9826398ae0a40bcc1855a455a
simps4simps <savarimani6@gmail.com>
2024-03-15 17:59:27 +0330

    Fetch from class-info endpoint

--- src/components/Archive/Archive.tsx
+++ src/components/Archive/Archive.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import "./Archive.css";
 
+interface Class {
+  name: string;
+}
+
 const Archive = () => {
-  return <div>Archive</div>;
+  const [classes, setClasses] = useState<Class[]>([] as Class[]);
+
+  const fetchClasses = async () => {
+    const result = await fetch("http://localhost:5000/class-info", {
+      credentials: "include",
+      method: "GET",
+    });
+    const resultJson: Class[] = await result.json();
+
+    setClasses(resultJson);
+  };
+
+  useEffect(() => {
+    fetchClasses();
+  }, [fetchClasses]);
+
+  return <div>{classes.map((res) => res.name)}</div>;
 };
 
 export default Archive;
